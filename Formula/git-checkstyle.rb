a6a19fcd1be0f4c7b02f6dd1cf56d064f49050c2
Liu Xiang <liuxiang@ktjr.com>
2021-02-26 13:51:22 +0800

    Update

--- Formula/git-checkstyle.rb
+++ Formula/git-checkstyle.rb
@@ -3,7 +3,7 @@ class GitCheckstyle < Formula
   homepage "https://gitlab.ktjr.com/open/cli/git-checkstyle"
   url "https://gitlab.ktjr.com/open/cli/git-checkstyle/repository/archive.zip?ref=master"
   sha256 "1922043cca9626f3324a58a023a229f3356a1dced5088a910066db581e31cfe7"
-  version "1.0.0"
+  version "1.0.1"
 
   depends_on "ktjrtech/utils/checkstyle88" => :recommended
 
