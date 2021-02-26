dcb0ddd9c5fd06df63396fe2614cdb6716cb604c
Liu Xiang <liuxiang@ktjr.com>
2021-02-26 10:18:16 +0800

    Add formula for checkstyle 8.8

---
+++ checkstyle88.rb
@@ -0,0 +1,26 @@
+class Checkstyle88 < Formula
+  desc "Check Java source against a coding standard"
+  homepage "https://checkstyle.sourceforge.io/"
+  url "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.8/checkstyle-8.8-all.jar"
+  sha256 "eff40175e846842f35825464f3d4dbdbe890803163b22a2fd8cd1b38aae50536"
+  license "LGPL-2.1-or-later"
+
+  bottle :unneeded
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "checkstyle-#{version}-all.jar"
+    bin.write_jar_script libexec/"checkstyle-#{version}-all.jar", "checkstyle"
+  end
+
+  test do
+    path = testpath/"foo.java"
+    path.write "public class Foo{ }\n"
+
+    output = shell_output("#{bin}/checkstyle -c /sun_checks.xml #{path}", 2)
+    errors = output.lines.select { |line| line.start_with?("[ERROR] #{path}") }
+    assert_match "#{path}:1:17: '{' is not preceded with whitespace.", errors.join(" ")
+    assert_equal errors.size, $CHILD_STATUS.exitstatus
+  end
+end
