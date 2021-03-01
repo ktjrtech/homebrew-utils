6cfad7d63e1e85d1ed59611e87ae2b5ef597e1bc
Liu Xiang <liuxiang@ktjr.com>
2021-03-01 23:01:51 +0800

    Add manual

--- Formula/git-checkstyle.rb
+++ Formula/git-checkstyle.rb
@@ -2,7 +2,7 @@ class GitCheckstyle < Formula
   desc "git checkstyle utils"
   homepage "https://gitlab.ktjr.com/open/cli/git-checkstyle"
   url "https://gitlab.ktjr.com/open/cli/git-checkstyle/repository/archive.zip?ref=1.1.1"
-  sha256 "670bf7ba977b768331466dd4d5788241bb60012a05401b4ba95d39ab7cc4329a"
+  sha256 "82e6e5891caa648bddccca4584babaa0a26783df8c6944246501859e35b4b9f9"
   version "1.1.1"
 
   depends_on "ktjrtech/utils/checkstyle88" => :recommended
@@ -11,5 +11,6 @@ class GitCheckstyle < Formula
   def install
     bin.install 'git-checkstyle'
     lib.install 'ktjr-git-checkstyle-lib'
+    system "cp -f git-checkstyle.1 /usr/local/share/man/man1/"
   end
 end
