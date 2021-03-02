ecc62099f13cc735c63d36407a38a79dfa6342b8
Liu Xiang <liuxiang@ktjr.com>
2021-03-02 12:17:53 +0800

    Update to 1.1.2

--- Formula/git-checkstyle.rb
+++ Formula/git-checkstyle.rb
@@ -1,12 +1,13 @@
 class GitCheckstyle < Formula
   desc "git checkstyle utils"
   homepage "https://gitlab.ktjr.com/open/cli/git-checkstyle"
-  url "https://gitlab.ktjr.com/open/cli/git-checkstyle/repository/archive.zip?ref=1.1.1"
-  sha256 "82e6e5891caa648bddccca4584babaa0a26783df8c6944246501859e35b4b9f9"
-  version "1.1.1"
+  url "https://gitlab.ktjr.com/open/cli/git-checkstyle/repository/archive.zip?ref=1.1.2"
+  sha256 "961d47c9acae351762aed79ade164971c00b0d7e940544e9c9eb6a2559144f33"
+  version "1.1.2"
 
   depends_on "ktjrtech/utils/checkstyle88" => :recommended
   depends_on "ktjrtech/utils/p3c-pmd" => :recommended
+  depends_on "fzf" => :recommended
 
   def install
     bin.install 'git-checkstyle'
